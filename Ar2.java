38bbd1c5dc8ca345b16d6e69b9473a73753bf32d
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2021-12-16 16:16:40 +0530

    Add files via upload

---
+++ Ar2.java
@@ -0,0 +1,17 @@
+class Ar2{
+	public static void main (String[] as){
+		short bat[]={129,223,1093,2345,2678,5543,6897,7896,8088,8348};
+		System.out.println(bat[9]);
+		System.out.println(bat[1]);
+		System.out.println(bat[4]);
+		System.out.println(bat[8]);
+		System.out.println(bat[5]);
+		System.out.println(bat[3]);
+		System.out.println(bat[2]);
+		System.out.println(bat[6]);
+		System.out.println(bat[8]);
+		System.out.println(bat[7]);
+
+	}
+
+}
