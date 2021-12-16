77d96bfa029301b458526bca1f470bc5a624f38e
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-16 12:44:02 +0530

    today will upload file

---
+++ Ar1.java
@@ -0,0 +1,16 @@
+class Ar1{
+ static byte []ant ={2,3,4,6,8,11,45,33,122,126};
+	public static void main(String []as){
+		
+		System.out.println(ant[2]);
+		System.out.println(ant[6]);
+		System.out.println(ant[0]);
+		System.out.println(ant[3]);
+		System.out.println(ant[9]);
+		System.out.println(ant[7]);
+		System.out.println(ant[8]);
+		System.out.println(ant[1]);
+		System.out.println(ant[4]);
+	}
+
+}
